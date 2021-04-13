4bb50c3a55618f96da7eef0385262605781a68f4
Paul Cervov <paulcervov@gmail.com>
2021-04-13 06:49:34 +0300

    Add bootstrap css and vue-moment

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vueMoment from 'vue-moment'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
+Vue.use(vueMoment);
 Vue.config.productionTip = false
 
 new Vue({
